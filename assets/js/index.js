6b6a2222500b477317726ffc7f0b34d3f49ea305
Acharya Group <acharyasumit786@gmail.com>
2025-11-27 12:10:34 +0530

    add marquee slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,4 +23,42 @@ menuBtn.addEventListener("click", () => {
 
 // dynmic year
  document.getElementById("year").textContent = new Date().getFullYear();
-    
+
+
+
+  const texts = [
+    "Welcome to Fiesta PR Agency",
+    "We Build Your Brand Visibility",
+    "Creative PR & Digital Strategy"
+  ];
+
+  let textIndex = 0;
+  let charIndex = 0;
+  const speed = 80;       
+  const eraseSpeed = 40;  
+  const delay = 1500;    
+
+  function typeEffect() {
+    const element = document.getElementById("typing-text");
+    if (charIndex < texts[textIndex].length) {
+      element.innerHTML += texts[textIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(typeEffect, speed);
+    } else {
+      setTimeout(eraseEffect, delay);
+    }
+  }
+
+  function eraseEffect() {
+    const element = document.getElementById("typing-text");
+    if (charIndex > 0) {
+      element.innerHTML = texts[textIndex].substring(0, charIndex - 1);
+      charIndex--;
+      setTimeout(eraseEffect, eraseSpeed);
+    } else {
+      textIndex = (textIndex + 1) % texts.length; // next text
+      setTimeout(typeEffect, 300);
+    }
+  }
+
+  window.onload = typeEffect;
